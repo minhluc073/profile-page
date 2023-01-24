eb6a9fe7302ab438436fa64266f209469f8dbd96
minhluc073 <nguyenminhluc0703@gmail.com>
2023-01-24 22:27:26 +0700

    cmt

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
+  //++++++
 );
 root.render(<App />);
-
